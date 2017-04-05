b8d59ff9430e4167f7e281b9f77a364a2489929b
Leonardo Filipe <leonardo.filipe@live.com>
2017-04-05 20:32:38 -0300

    Store mean orientation for labels.

--- Label.py
+++ Label.py
@@ -6,9 +6,17 @@ class Label(object):
         self.number = labelNumber
         self.parentList = parentList
         self.color = (rnd.randint(0,255), rnd.randint(0, 255), rnd.randint(0, 255))
+        self.orientationSum = 0.0
     
     def assignTo(self, obj):
         if obj.label != None:
             raise ValueError('Trying to assign new Label to an object that already has a label.')
         obj.label = self
         self.count += 1
+        self.orientationSum += obj.orientation
+
+    def getMeanOrientation(self):
+        if self.count != 0:
+            return self.orientationSum / self.count
+        else:
+            return 0.0
